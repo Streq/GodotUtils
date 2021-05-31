89f0d18bf49b2d4f9d9513c005bcef9995c73e8e
streq <strequena@gmail.com>
2021-05-31 00:56:41 -0300

    textbox

--- RopeJoint2D/RigidBody2D.gd
+++ RopeJoint2D/RigidBody2D.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
+export (float) var speed
 
 func _physics_process(delta):
 	var dir = Vector2(float(Input.is_action_pressed("ui_right")) - float(Input.is_action_pressed("ui_left")), float(Input.is_action_pressed("ui_down")) - float(Input.is_action_pressed("ui_up"))).normalized()
-	apply_central_impulse(dir*1000)
+	apply_central_impulse(dir*speed)
